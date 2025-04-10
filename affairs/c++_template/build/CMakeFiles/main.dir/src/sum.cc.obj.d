dd539ff43543d6fe23bc06453d30a0ce3ffc712a
LFNL-scholar <lfnltech@163.com>
2025-04-10 23:27:51 +0800

    删除build

--- affairs/c++_template/build/CMakeFiles/main.dir/src/sum.cc.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/src/sum.cc.obj: \
- E:\A-LFNL-Data\LFNL-25-2\MicroCore-Insight\affairs\c++_template\src\sum.cc \
- E:/A-LFNL-Data/LFNL-25-2/MicroCore-Insight/affairs/c++_template/include/sum.h
