584018638aaaaf1854812f0d76ffff914cacb592
李健豪 <lijianhao@iyunwen.com>
2025-04-10 17:59:25 +0800

    崩溃了

---
+++ affairs/c++/simple/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sum.h"
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    int result = sum(a, b);
+
+    std::cout << "The sum of " << a << " and " << b << " is: " << result << std::endl;
+
+    return 0;
+}
