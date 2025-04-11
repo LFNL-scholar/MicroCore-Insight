840d56c1f75c2e606963c33cbb601e3c9402f751
李健豪 <lijianhao@iyunwen.com>
2025-04-11 11:37:11 +0800

    更新C++

---
+++ affairs/c++/main/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int sum = 5;
+    sum = sum + a + b;
+    std::cout<<sum<<std::endl;
+    std::cout<<"YUNWEN"<<std::endl;
+    return 0;
+}
--- affairs/c++/simple/sum.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUM_H
-#define SUM_H
-
-// 函数声明
-int sum(int a, int b);
-
-#endif
