1d23ce46955687b064aa4799047627409d837115
李健豪 <lijianhao@iyunwen.com>
2025-04-16 11:38:34 +0800

    新增文件

--- affairs/en-project-2/app/routers/register_routes.py
+++ affairs/en-project-2/app/routers/register_routes.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api import tts
+from api import asr
 
 def register_routes(app: FastAPI):
     """注册所有路由"""
+    app.include_router(asr.router)
     app.include_router(tts.router)
